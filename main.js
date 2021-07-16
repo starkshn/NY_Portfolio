756d452110a8a374173a6ad4cf64085e1d0752ff
CJB_ny <74084819+starkshn@users.noreply.github.com>
2021-07-16 19:48:10 +0900

    js make navbar menu moving
    js make navbar menu moving

--- main.js
+++ main.js
@@ -15,3 +15,43 @@ document.addEventListener('scroll', () => {
         navbar_id.classList.remove('navbar__transparent');
     }
 });
+
+// Make navbar when click scroll to what you click(my_work)
+// const navbarMenu = document.querySelector('.navbar__menu');
+// navbarMenu.addEventListener('click', (event) => {
+    
+//     const target = event.target;
+//     const link = target.dataset.link;
+    
+//     if (link == null) {
+//         return;
+//     }
+//     console.log(target.dataset.link);
+
+//     const targetY = document.getElementById(target.dataset.link);
+//     const targetY_start = targetY.getBoundingClientRect().height;
+//     const targetY_end = targetY.getBoundingClientRect().bottom;
+//     const targetY_height = targetY_end - targetY_start;
+
+//     window.scrollTo({top: targetY_height, left:0, behavior:'smooth'});
+
+// })
+
+
+// Handle scrolling when tapping on the navbar menu
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (event) => {
+    
+    const target = event.target;
+    const link = target.dataset.link;
+    
+    if (link == null) {
+        return;
+    }
+    console.log(target.dataset.link);
+
+    const scrollTo = document.getElementById(link);
+    scrollTo.scrollIntoView({behavior: 'smooth'});
+    
+
+});
