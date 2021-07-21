cf941cc4070b44371926c1f156bbc0daf730ba73
NY <startkshn97@gmail.com>
2021-07-21 17:50:13 +0900

    js arrow btn
    make js arrow btn

--- main.js
+++ main.js
@@ -73,44 +73,106 @@ move_to_contact.addEventListener('click', () =>{
 // });
 
 // make about transparent when scroll
-document.addEventListener('scroll', () => {
+// document.addEventListener('scroll', () => {
     
-    const about = document.querySelector('#about');
-    const about_y = about.getBoundingClientRect().height;
-    const about_bottom = about.getBoundingClientRect().bottom;
+//     const about = document.querySelector('#about');
+//     const about_y = about.getBoundingClientRect().height;
+//     const about_bottom = about.getBoundingClientRect().bottom;
 
-    if (about_y / 2 > about_bottom) {
-        about.classList.add('transparent');
-    } else {
-        about.classList.remove('transparent');
-    }
+//     if (about_y / 2 > about_bottom) {
+//         about.classList.add('transparent');
+//     } else {
+//         about.classList.remove('transparent');
+//     }
     
-});
+// });
 
-const about = document.querySelector('#about');
-const about_y = about.getBoundingClientRect().height;
-const about_bottom = about.getBoundingClientRect().bottom;
+// const about = document.querySelector('#about');
+// const about_y = about.getBoundingClientRect().height;
+// const about_bottom = about.getBoundingClientRect().bottom;
 
-console.log(about_y);
-console.log(about_bottom);
-console.log(window.scrollY);
+// console.log(about_y);
+// console.log(about_bottom);
+// console.log(window.scrollY);
 
-console.log(about.getBoundingClientRect().height);
+// console.log(about.getBoundingClientRect().height);
 
-// test
-document.addEventListener('scroll', () => {
+// // test
+// document.addEventListener('scroll', () => {
     
     // console.log(window.scrollY);
     
 
-});
+// });
 
 // make home slowly fade to transparent as the window scrolls down.home_contact
-
-
 const home = document.querySelector('.home__container');
 const homeHeight = home.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
-    console.log(1 - window.scrollY / homeHeight);
+    // console.log(1 - window.scrollY / homeHeight);
     home.style.opacity = 1 - window.scrollY / homeHeight;
 })
+
+// make arrow btn
+arrow = document.querySelector('.arrow__btn');
+arrow.addEventListener('click', () => {
+    scrollIntoView('#Home');
+})
+
+document.addEventListener('scroll', () => {
+    if(homeHeight / 2 < window.scrollY){
+        arrow.classList.add('visible');
+    } else{
+        arrow.classList.remove('visible');
+    }
+});
+
+
+// make scroll bar & when meet section add text box
+// section_height_list = [];
+// home_123 = document.querySelector('#Home').getBoundingClientRect().height;
+// section_height_list[1] = document.querySelector('#about').getBoundingClientRect().height;
+// section_height_list[2] = document.querySelector('#skills').getBoundingClientRect().height;
+// section_height_list[3] = document.querySelector('#work').getBoundingClientRect().height;
+// section_height_list[4] = document.querySelector('#testimonials').getBoundingClientRect().height;
+// section_height_list[5] = document.querySelector('#contact').getBoundingClientRect().height;
+
+// scrollbar = document.querySelector('beScrollbar');
+
+// scroll_box = document.querySelector('.scroll_box');
+
+
+// document.addEventListener('scroll', () => {
+
+//     console.log(home_123 - 100);
+//     console.log(window.scrollY);
+
+//     if (home_123 - 500 < window.scrollY){
+//         scroll_box.style.background = "tomato";
+//     }
+//     else {
+//         scroll_box.style.background = "black";
+//     }
+    
+
+// });
+
+
+
+
+// document.addEventListener('scroll', () => {
+//     Scroll_y = window.scrollY;
+//     while (section_height_list = true) {
+//         for(i = 0; i < 6; i++) {
+//             if(section_height_list[0] - 100 < Scroll_y) {
+//                 scroll_box = document.querySelector('scroll_box');
+//                 scroll_box.style.color = blue;
+
+//             }
+//             else {
+
+//             }
+//         }
+//     }
+    
+// });
